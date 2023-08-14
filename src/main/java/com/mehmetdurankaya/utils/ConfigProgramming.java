6488c2a4b946da8a3c58a5e6ab1bba69edc20c7d
Mehmet Duran Kaya <mehmetdurankaya@gmail.com>
2023-08-14 13:10:53 +0300

    DevTools Aktif etmek

---
+++ src/main/java/com/mehmetdurankaya/utils/ConfigProgramming.java
@@ -0,0 +1,7 @@
+package com.mehmetdurankaya.utils;
+
+public class ConfigProgramming {
+    //DevTools ==> LiveReload
+    // file ==> settings ==> compiler ==> Build project outomatically(Ctrl+alt+s)
+    // file ==> Adcanced Setting ==> Allow Auto-make to_start even(OK clicked) // bu ayar yapılmaz ise devtols yapısı çalışmaz
+}
