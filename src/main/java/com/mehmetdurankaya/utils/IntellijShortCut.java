5657a0401b6a9f45a5cb00921bc287483b5d1314
Mehmet Duran Kaya <mehmetdurankaya@gmail.com>
2023-08-14 13:09:34 +0300

    kısayollar

---
+++ src/main/java/com/mehmetdurankaya/utils/IntellijShortCut.java
@@ -0,0 +1,8 @@
+package com.mehmetdurankaya.utils;
+
+public class IntellijShortCut {
+//    Setting ===> ctr+alt+s
+//    Alt+Ins===> constructor, toString, getter,setter
+//    Ctrl+shift+/ ==> multiple coomment
+
+}
