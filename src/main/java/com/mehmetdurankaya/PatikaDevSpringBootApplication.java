7d9d870f59ca222add4c481a5021dfb07fef8508
Mehmet Duran Kaya <mehmetdurankaya@gmail.com>
2023-08-14 13:09:07 +0300

    SpringBootApplication==> security (exclude )

--- src/main/java/com/mehmetdurankaya/PatikaDevSpringBootApplication.java
+++ src/main/java/com/mehmetdurankaya/PatikaDevSpringBootApplication.java
@@ -3,7 +3,11 @@ package com.mehmetdurankaya;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication( exclude ={
+        org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration.class,
+        org.springframework.boot.actuate.autoconfigure.security.servlet.ManagementWebSecurityAutoConfiguration.class
+})
+
 public class PatikaDevSpringBootApplication {
 
     public static void main(String[] args) {
