d7e69ea755e7538a337bcc88e3d9d7f481713318
Mehmet Duran Kaya <mehmetdurankaya@gmail.com>
2023-08-14 14:54:16 +0300

    Bean controller

---
+++ src/main/java/com/mehmetdurankaya/controller/BeanController.java
@@ -0,0 +1,21 @@
+package com.mehmetdurankaya.controller;
+
+import com.mehmetdurankaya.bean.BeanConfig;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class BeanController {
+    @Autowired
+    BeanConfig beanConfig;
+
+
+   //  http://localhost:8080/bean/beanDto
+    @GetMapping("/bean/beanDto")// url
+    @ResponseBody // html olmadan ekranda göstermeye yarar
+    public String getBeanDto(){
+        return beanConfig.beanDto()+".";
+    }
+}
