cec63c63fbb1985e676e19c07beeaabf0ed093d6
Mehmet Duran Kaya <mehmetdurankaya@gmail.com>
2023-08-14 08:17:55 +0300

    firs commit(PatikaDev)

---
+++ src/main/java/com/mehmetdurankaya/PatikaDevSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.mehmetdurankaya;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PatikaDevSpringBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PatikaDevSpringBootApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/mehmetdurankaya/PatikaDevSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mehmetdurankaya;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PatikaDevSpringBootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
