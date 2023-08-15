c1e99d85e4c184ee7769ee77260465f31f872b6a
Mehmet Duran Kaya <mehmetdurankaya@gmail.com>
2023-08-15 16:48:44 +0300

    thymeleaf yapısı ve kullanımı

--- src/main/java/com/mehmetdurankaya/bean/PostContructTutorials.java
+++ src/main/java/com/mehmetdurankaya/bean/PostContructTutorials.java
@@ -7,20 +7,20 @@ import javax.annotation.PostConstruct;
 import java.util.logging.Logger;
 
 public class PostContructTutorials {
-    @Autowired
-    Logger LOG;
+//    @Autowired
+//    Logger LOG;
     //    public BeanController() {
 //        LOG.info("Log info çağrıldı");
 //    }
 
     // bean henüz başlamadığından null alıyoruz ve burada bağımlılığı enjecte edemiyoruz.
     //Görevi : bir bean nesnesi oluşturulduğunda hemen oluşur.
-    @PostConstruct
-    public void init(){
-        LOG.info("Log info çağrıldı");
-    }
-    public static void main(String[] args){
-        PostContructTutorials postContructTutorials=new PostContructTutorials();
-        System.out.println(postContructTutorials);
-    }
+//    @PostConstruct
+//    public void init(){
+//        LOG.info("Log info çağrıldı");
+//    }
+//    public static void main(String[] args){
+//        PostContructTutorials postContructTutorials=new PostContructTutorials();
+//        System.out.println(postContructTutorials);
+//    }
 }
---
+++ src/main/java/com/mehmetdurankaya/controller/ThymeleafController.java
@@ -0,0 +1,17 @@
+package com.mehmetdurankaya.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class ThymeleafController {
+
+//http://localhost:8080/thymeleaf1
+@GetMapping("/thymeleaf1")
+//@ResponseBody
+public String getThymeleaf1(){
+
+return "thymeleaf1";
+}
+}
